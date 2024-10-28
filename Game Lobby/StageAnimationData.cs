0823ba656f80101d519ed7377eeb4b11676f99f1
Hyeon O <115928656+gusdh8380@users.noreply.github.com>
2024-10-28 17:00:43 +0900

    Delete Game Lobby directory

--- Game Lobby/StageAnimationData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "StageAnimationData", menuName = "ScriptableObjects/StageAnimationData", order = 1)]
-public class StageAnimationData : ScriptableObject {
-    public GameManager.Stage stage;
-    public RuntimeAnimatorController animatorController;
-}
